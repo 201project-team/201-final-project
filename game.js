5d61fd2be2c4dd06dcf7d8ee36810db303ac6e0d
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-22 11:17:07 -0700

    refactored code, getting src element bug

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
 var randomWord;
 var wordList = ['atlanta', 'falcons', 'superbowl', 'hawks', 'skywalker'];
+
 function generateRandomWord() {
   for (var i = 0; i < wordList.length; i++) {
-    var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     var wordBoxReference = document.getElementById('word-box');
     wordBoxReference.textContent = randomWord;
-
-    var input = document.querySelector('input');
-    var log = document.getElementById('user-text');
-    input.addEventListener('input', updateValue);
-
-    function updateValue(event) {
-      log.textContent = event.srcElement.value;
-      if (log.textContent === randomWord) {
-        generateRandomWord();
-        input.value = "";
-        log.value = "";
-      }
-    }
   }
 };
+
+function updateValue(event) {
+  var input = document.querySelector('input');
+  var log = document.getElementById('user-text');
+  input.addEventListener('input', updateValue);
+  log.textContent = event.srcElement.value;
+  if (log.textContent === randomWord) {
+    generateRandomWord();
+    input.value = "";
+  }
+}
 generateRandomWord();
+updateValue();
