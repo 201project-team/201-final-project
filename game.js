19f5b474808e09930097253808b3dd71bdfa4dcb
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-21 21:04:58 -0700

    text will now clear after word is correctly typed

--- game.js
+++ game.js
@@ -3,19 +3,21 @@ var wordList = ['atlanta', 'falcons', 'superbowl', 'hawks', 'skywalker'];
 function generateRandomWord() {
   for (var i = 0; i < wordList.length; i++) {
     var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-    var wordBoxReference = document.getElementById('word-box')
+    var wordBoxReference = document.getElementById('word-box');
     wordBoxReference.textContent = randomWord;
+
     var input = document.querySelector('input');
-    var log = document.getElementById('values');
+    var log = document.getElementById('user-text');
     input.addEventListener('input', updateValue);
 
     function updateValue(event) {
       log.textContent = event.srcElement.value;
       if (log.textContent === randomWord) {
         generateRandomWord();
+        input.value = "";
+        log.value = "";
       }
     }
   }
 };
 generateRandomWord();
-
