0227890a8ac538dbee4f9592a094f125cdb33c5d
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-25 01:30:09 -0700

    added insanity difficulty, high score section, keyboard sounds, music

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 'use strict'
 
-  var easy = ["ACRES", "ACT", "BASIC", "BASIS", "BAT", "BE", "BEAN", "BEAR", "BEAT", "BEE", "BEEN", "BEGAN", "BEGUN", "CHART", "CHECK", "CHEST", "CHIEF", "CHILD", "CROP", "CROSS", "CROWD", "CRY", "CUP", "CURVE", "CUT", "DAILY", "DANCE", "DARK", "DATE", "DAWN", "DAY", "DEAD", "DEAL", "DEAR", "DEATH", "DEEP", "DEER", "DEPTH", "DESK", "EXIST", "EXTRA", "EYE", "FACE", "FACT", "FAIR", "FALL", "FAR", "FARM", "FAST", "FAT", "FEAR", "FED", "FEED", "FEEL", "FEET", "FELL", "FELT", "FENCE", "FEW", "FEWER", "FIELD", "GOT", "GRADE", "GRAIN", "GRAPH", "GRASS", "GRAY", "GREAT", "GREEN", "GREW", "GROUP", "GROW", "GROWN", "GUARD", "GUESS", "GUIDE", "GULF", "GUN", "HABIT", "HAD", "HAIR", "HALF", "HALL", "HAND", "HANG", "HAPPY", "HARD", "HAS", "HAT", "INTO", "IRON", "IS", "IT", "ITS", "JACK", "JAR", "JET", "JOB", "JOIN", "JOY", "JUDGE", "JUMP", "JUST", "KEEP", "KEPT", "KEY", "KIDS", "KILL", "KIND", "KNEW", "KNIFE", "KNOW", "KNOWN", "LABEL", "LABOR", "LACK", "LADY", "LAID", "LAKE", "LAMP", "LAND", "LARGE", "LAST", "LATE", "LATER", "LAUGH", "LAW", "LAY", "MIND", "MINE", "NUTS", "OCCUR", "OCEAN", "OF", "OFF", "OFFER", "OIL", "PUSH", "PUT", "QUEEN", "QUICK", "QUIET", "QUITE", "RACE", "RADIO", "RAIN", "RAISE", "RAN", "RANCH", "RANGE", "RATE", "RAW", "RAYS", "REACH", "READ", "READY", "REAL", "SUIT", "SUM", "SUN", "SURE", "SWAM", "SWEET", "SWEPT", "SWIM", "SWING", "SWUNG", "TABLE", "TAIL", "TAKE", "TAKEN", "TALES", "TALK", "TALL", "TANK", "TAPE", "TASK", "TASTE", "TAX", "TEA", "TEACH", "TURN", "TWICE", "TWO", "TYPE", "UNCLE", "UNDER", "UNION", "UNIT", "UNTIL", "UP", "UPON", "UPPER", "US", "VIEW", "VISIT", "VOICE", "VOTE", "VOWEL", "WAGON", "WAIT", "WALK", "WALL", "WANT", "WAR", "WARM", "WARN", "WAS", "WASH", "WASTE", "WATCH", "WATER", "WAVE", "WAY", "WE", "WEAK", "WEAR", "WEEK", "WRITE", "WRONG", "WROTE", "YARD", "YEAR", "YES", "YET", "YOU", "YOUNG", "YOUR", "YOUTH", "ZERO", "ZOO"]
+var easy = ["ACRES", "ACT", "BASIC", "BASIS", "BAT", "BE", "BEAN", "BEAR", "BEAT", "BEE", "BEEN", "BEGAN", "BEGUN", "CHART", "CHECK", "CHEST", "CHIEF", "CHILD", "CROP", "CROSS", "CROWD", "CRY", "CUP", "CURVE", "CUT", "DAILY", "DANCE", "DARK", "DATE", "DAWN", "DAY", "DEAD", "DEAL", "DEAR", "DEATH", "DEEP", "DEER", "DEPTH", "DESK", "EXIST", "EXTRA", "EYE", "FACE", "FACT", "FAIR", "FALL", "FAR", "FARM", "FAST", "FAT", "FEAR", "FED", "FEED", "FEEL", "FEET", "FELL", "FELT", "FENCE", "FEW", "FEWER", "FIELD", "GOT", "GRADE", "GRAIN", "GRAPH", "GRASS", "GRAY", "GREAT", "GREEN", "GREW", "GROUP", "GROW", "GROWN", "GUARD", "GUESS", "GUIDE", "GULF", "GUN", "HABIT", "HAD", "HAIR", "HALF", "HALL", "HAND", "HANG", "HAPPY", "HARD", "HAS", "HAT", "INTO", "IRON", "IS", "IT", "ITS", "JACK", "JAR", "JET", "JOB", "JOIN", "JOY", "JUDGE", "JUMP", "JUST", "KEEP", "KEPT", "KEY", "KIDS", "KILL", "KIND", "KNEW", "KNIFE", "KNOW", "KNOWN", "LABEL", "LABOR", "LACK", "LADY", "LAID", "LAKE", "LAMP", "LAND", "LARGE", "LAST", "LATE", "LATER", "LAUGH", "LAW", "LAY", "MIND", "MINE", "NUTS", "OCCUR", "OCEAN", "OF", "OFF", "OFFER", "OIL", "PUSH", "PUT", "QUEEN", "QUICK", "QUIET", "QUITE", "RACE", "RADIO", "RAIN", "RAISE", "RAN", "RANCH", "RANGE", "RATE", "RAW", "RAYS", "REACH", "READ", "READY", "REAL", "SUIT", "SUM", "SUN", "SURE", "SWAM", "SWEET", "SWEPT", "SWIM", "SWING", "SWUNG", "TABLE", "TAIL", "TAKE", "TAKEN", "TALES", "TALK", "TALL", "TANK", "TAPE", "TASK", "TASTE", "TAX", "TEA", "TEACH", "TURN", "TWICE", "TWO", "TYPE", "UNCLE", "UNDER", "UNION", "UNIT", "UNTIL", "UP", "UPON", "UPPER", "US", "VIEW", "VISIT", "VOICE", "VOTE", "VOWEL", "WAGON", "WAIT", "WALK", "WALL", "WANT", "WAR", "WARM", "WARN", "WAS", "WASH", "WASTE", "WATCH", "WATER", "WAVE", "WAY", "WE", "WEAK", "WEAR", "WEEK", "WRITE", "WRONG", "WROTE", "YARD", "YEAR", "YES", "YET", "YOU", "YOUNG", "YOUR", "YOUTH", "ZERO", "ZOO"]
 
-  var medium = ["ACCOUNT", "ACCURATE", "ACROSS", "ACTION", "ACTIVE", "ACTIVITY", "BASEBALL", "BASKET", "BATTLE", "BEAUTY", "BECAME", "BECAUSE", "BECOME", "BECOMING", "BEFORE", "BEHAVIOR", "BEHIND", "CHANCE", "CHANGE", "CHANGING", "CHAPTER", "CHARGE", "CHEESE", "CHEMICAL", "CHICKEN", "CHILDREN", "CURIOUS", "CURRENT", "CUSTOMS", "CUTTING", "DAMAGE", "DANGER", "DARKNESS", "DAUGHTER", "DECIDE", "DECLARED", "DEEPLY", "DEGREE", "DEPEND", "DESCRIBE", "DESERT", "DESIGN", "DETAIL", "EXCEPT", "EXCHANGE", "EXCITED", "EXCITING", "EXERCISE", "EXPECT", "EXPLAIN", "EXPLORE", "EXPRESS", "FACING", "FACTOR", "FACTORY", "FAILED", "FAIRLY", "FALLEN", "FAMILIAR", "FAMILY", "FAMOUS", "FARMER", "FARTHER", "FASTENED", "FASTER", "FATHER", "FAVORITE", "FEATHERS", "FEATURE", "FELLOW", "GRABBED", "GRAVITY", "GREATER", "GREATEST", "GREATLY", "GROUND", "GROWTH", "HALFWAY", "HANDLE", "HANDSOME", "HAPPEN", "HAPPENED", "HAPPILY", "HARBOR", "HARDER", "HARDLY", "INSTANCE", "INSTANT", "INSTEAD", "INTEREST", "INTERIOR", "INVENTED", "INVOLVED", "ISLAND", "ITSELF", "JOINED", "JOURNEY", "JUNGLE", "KITCHEN", "LANGUAGE", "LARGER", "LARGEST", "LAYERS", "MINERALS", "MINUTE", "MIRROR", "MISSING", "MISSION", "MISTAKE", "NUMBER", "NUMERAL", "OBJECT", "OBSERVE", "OBTAIN", "OFFICE", "OFFICER", "OFFICIAL", "PURPOSE", "PUTTING", "QUARTER", "QUESTION", "QUICKLY", "QUIETLY", "RABBIT", "RAILROAD", "RAPIDLY", "RATHER", "READER", "REALIZE", "SUGGEST", "SUMMER", "SUNLIGHT", "SUPPER", "SUPPLY", "SUPPORT", "SUPPOSE", "SURFACE", "SURPRISE", "SWIMMING", "SYLLABLE", "SYMBOL", "SYSTEM", "TAUGHT", "TEACHER", "TWELVE", "TWENTY", "TYPICAL", "UNHAPPY", "UNIVERSE", "UNKNOWN", "UNLESS", "UNUSUAL", "UPWARD", "VESSELS", "VICTORY", "VILLAGE", "VISITOR", "VOLUME", "VOYAGE", "WEALTH", "WEATHER", "WRAPPED", "WRITER", "WRITING", "WRITTEN", "YELLOW", "YOUNGER", "YOURSELF"];
-  
+var medium = ["ACCOUNT", "ACCURATE", "ACROSS", "ACTION", "ACTIVE", "ACTIVITY", "BASEBALL", "BASKET", "BATTLE", "BEAUTY", "BECAME", "BECAUSE", "BECOME", "BECOMING", "BEFORE", "BEHAVIOR", "BEHIND", "CHANCE", "CHANGE", "CHANGING", "CHAPTER", "CHARGE", "CHEESE", "CHEMICAL", "CHICKEN", "CHILDREN", "CURIOUS", "CURRENT", "CUSTOMS", "CUTTING", "DAMAGE", "DANGER", "DARKNESS", "DAUGHTER", "DECIDE", "DECLARED", "DEEPLY", "DEGREE", "DEPEND", "DESCRIBE", "DESERT", "DESIGN", "DETAIL", "EXCEPT", "EXCHANGE", "EXCITED", "EXCITING", "EXERCISE", "EXPECT", "EXPLAIN", "EXPLORE", "EXPRESS", "FACING", "FACTOR", "FACTORY", "FAILED", "FAIRLY", "FALLEN", "FAMILIAR", "FAMILY", "FAMOUS", "FARMER", "FARTHER", "FASTENED", "FASTER", "FATHER", "FAVORITE", "FEATHERS", "FEATURE", "FELLOW", "GRABBED", "GRAVITY", "GREATER", "GREATEST", "GREATLY", "GROUND", "GROWTH", "HALFWAY", "HANDLE", "HANDSOME", "HAPPEN", "HAPPENED", "HAPPILY", "HARBOR", "HARDER", "HARDLY", "INSTANCE", "INSTANT", "INSTEAD", "INTEREST", "INTERIOR", "INVENTED", "INVOLVED", "ISLAND", "ITSELF", "JOINED", "JOURNEY", "JUNGLE", "KITCHEN", "LANGUAGE", "LARGER", "LARGEST", "LAYERS", "MINERALS", "MINUTE", "MIRROR", "MISSING", "MISSION", "MISTAKE", "NUMBER", "NUMERAL", "OBJECT", "OBSERVE", "OBTAIN", "OFFICE", "OFFICER", "OFFICIAL", "PURPOSE", "PUTTING", "QUARTER", "QUESTION", "QUICKLY", "QUIETLY", "RABBIT", "RAILROAD", "RAPIDLY", "RATHER", "READER", "REALIZE", "SUGGEST", "SUMMER", "SUNLIGHT", "SUPPER", "SUPPLY", "SUPPORT", "SUPPOSE", "SURFACE", "SURPRISE", "SWIMMING", "SYLLABLE", "SYMBOL", "SYSTEM", "TAUGHT", "TEACHER", "TWELVE", "TWENTY", "TYPICAL", "UNHAPPY", "UNIVERSE", "UNKNOWN", "UNLESS", "UNUSUAL", "UPWARD", "VESSELS", "VICTORY", "VILLAGE", "VISITOR", "VOLUME", "VOYAGE", "WEALTH", "WEATHER", "WRAPPED", "WRITER", "WRITING", "WRITTEN", "YELLOW", "YOUNGER", "YOURSELF"];
 
-  var hard = ["ACCURATE", "ACTIVITY", "BASEBALL", "BEAUTIFUL", "BECOMING", "BEGINNING", "BEHAVIOR", "CHANGING", "CHARACTER", "CHARACTERISTIC", "CHEMICAL", "CHILDREN", "DANGEROUS", "DARKNESS", "DAUGHTER", "DECLARED", "DEFINITION", "DESCRIBE", "EXCELLENT", "EXCHANGE", "EXCITEMENT", "EXCITING", "EXCLAIMED", "EXERCISE", "EXPERIENCE", "EXPERIMENT", "EXPLANATION", "EXPRESSION", "FAMILIAR", "FASTENED", "FAVORITE", "FEATHERS", "GOVERNMENT", "GRADUALLY", "GRANDFATHER", "GRANDMOTHER", "GREATEST", "HANDSOME", "HAPPENED", "INSTANCE", "INSTRUMENT", "INTEREST", "INTERIOR", "INTRODUCED", "INVENTED", "INVOLVED", "KNOWLEDGE", "LANGUAGE", "MINERALS", "OCCASIONALLY", "OFFICIAL", "QUESTION", "RAILROAD", "SUNLIGHT", "SURPRISE", "SURROUNDED", "SWIMMING", "SYLLABLE", "UNDERLINE", "UNDERSTANDING", "UNIVERSE", "YESTERDAY", "YOURSELF"];
+var hard = ["ACCURATE", "ACTIVITY", "BASEBALL", "BEAUTIFUL", "BECOMING", "BEGINNING", "BEHAVIOR", "CHANGING", "CHARACTER", "CHARACTERISTIC", "CHEMICAL", "CHILDREN", "DANGEROUS", "DARKNESS", "DAUGHTER", "DECLARED", "DEFINITION", "DESCRIBE", "EXCELLENT", "EXCHANGE", "EXCITEMENT", "EXCITING", "EXCLAIMED", "EXERCISE", "EXPERIENCE", "EXPERIMENT", "EXPLANATION", "EXPRESSION", "FAMILIAR", "FASTENED", "FAVORITE", "FEATHERS", "GOVERNMENT", "GRADUALLY", "GRANDFATHER", "GRANDMOTHER", "GREATEST", "HANDSOME", "HAPPENED", "INSTANCE", "INSTRUMENT", "INTEREST", "INTERIOR", "INTRODUCED", "INVENTED", "INVOLVED", "KNOWLEDGE", "LANGUAGE", "MINERALS", "OCCASIONALLY", "OFFICIAL", "QUESTION", "RAILROAD", "SUNLIGHT", "SURPRISE", "SURROUNDED", "SWIMMING", "SYLLABLE", "UNDERLINE", "UNDERSTANDING", "UNIVERSE", "YESTERDAY", "YOURSELF"];
+
+var insanity = ["PHONOCARDIOGRAM", "METALINGUISTICS", "DETERMINATENESS", "NONMONISTICALLY", "PROLETARIANNESS", "PREORGANIZATION", "INTERPEDUNCULAR", "DERMATOPHYTOSIS", "NONARISTOCRATIC", "AUTECOLOGICALLY", "NONCONTROLLABLE", "NONEMANCIPATION", "RENORMALIZATION", "PRODISTRIBUTION", "NONDISCOVERABLE", "HYPEROXYGENIZED", "UNCOUNTERVAILED", "IRREFRAGABILITY", "WITHDRAWINGNESS", "CARPENTERSVILLE", "NONCONTINUATION", "SYLLABIFICATION", "RECONSOLIDATING", "BLAGOVESHCHENSK", "TREASONABLENESS", "TRANSACTIONALLY", "ANTIPERISTALTIC", "TRANSFILTRATION", "SUPERTEMPTATION", "ORONTIPHARYNGES", "PREINCORPORATED", "NONILLUSTRATIVE", "PSEUDORHEUMATIC", "NONDECASYLLABLE", "GYNAECOMORPHOUS", "ANACOLUTHICALLY", "NONINTERCEPTIVE", "THERMODIFFUSION", "HYPNOSPORANGIUM", "PHOTOMETRICALLY", "UNCONGRATULATED", "SUPERACCUMULATE", "OUTMALAPROPPING", "UNDEVELOPMENTAL", "SUPERORDINATING", "IRREPEALABILITY", "TERRITORIALIZED", "COLONIALIZATION", "UNEXPOSTULATING", "UNCONGLUTINATED"];
 
 var wordBoxReference = document.getElementById('word-box');
 var input = document.getElementById('input');
@@ -29,22 +30,25 @@ var storedUserScore = 0;
 var gameMusic = new Audio('assets/Good-Morning-Doctor-Weird.mp3');
 var correctAnswer = new Audio('assets/correct-answer-sound-effect-19.mp3');
 var incorrectAnswer = new Audio('assets/Quick Fart-SoundBible.com-655578646.mp3');
+var typewriterSound = new Audio('assets/typewriter-key-1.mp3');
+var insanityMusic = new Audio('assets/insanity.mp3');
 
 function generateRandomWord() {
-  if(userScore <=4){
-  randomWord = easy[Math.floor(Math.random() * easy.length)];
-  randomWordSplit = randomWord.split('');
-} else if(userScore >=5 && userScore <= 9){
-  randomWord = medium[Math.floor(Math.random() * medium.length)];
-  randomWordSplit = randomWord.split('');
-} else if(userScore >= 10){
-  randomWord = hard[Math.floor(Math.random() * hard.length)];
-  randomWordSplit = randomWord.split('');
-}
-// else if(userScore >= 20){
-//   randomWord = isanity[Math.floor(Math.random() * insanity.length)];
-//   randomWordSplit = randomWord.split('');
-// }
+  if (userScore <= 4) {
+    randomWord = easy[Math.floor(Math.random() * easy.length)];
+    randomWordSplit = randomWord.split('');
+  } else if (userScore >= 5 && userScore <= 9) {
+    randomWord = medium[Math.floor(Math.random() * medium.length)];
+    randomWordSplit = randomWord.split('');
+  } else if (userScore >= 10 && userScore < 20) {
+    randomWord = hard[Math.floor(Math.random() * hard.length)];
+    randomWordSplit = randomWord.split('');
+  }
+  if (userScore >= 20) {
+    insanityMode();
+    randomWord = insanity[Math.floor(Math.random() * insanity.length)];
+    randomWordSplit = randomWord.split('');
+  }
   for (var i = 0; i < randomWordSplit.length; i++) {
     var span = document.createElement('span')
     span.textContent = randomWordSplit[i];
@@ -62,22 +66,30 @@ function highlightLetters() {
       console.log(`${randomWordSplit[i]} is the same as ${textContentSplit[i]}`)
     }
     if (randomWordSplit[i] !== textContentSplit[i]) {
-      // incorrectAnswer.play();
     }
-    console.log(randomWordSplit[i] !== textContentSplit[i]);
   }
+  console.log(randomWordSplit[i] !== textContentSplit[i]);
 }
 
 function checkUserInput(event) {
   log.textContent = event.target.value.toUpperCase();
   if (log.textContent === randomWord) {
+    if (userScore >= 20) { // During insanity mode if word is correct add 3secs to timer
+      clearInterval(timer);
+      timer = setInterval(countdown, 1000);
+      timeleft = timeleft + 3;
+    }
     wordBoxReference.innerHTML = '';
-    correctAnswer.play();
-    generateRandomWord();
     input.value = "";
     userScore++;
-    playbackSpeed();
     document.getElementById('user-score').innerHTML = userScore;
+
+    generateRandomWord();
+
+    correctAnswer.play();
+    correctAnswer.currentTime = 0;
+    playbackSpeed();
+
   } else {
     highlightLetters();
   }
@@ -97,18 +109,18 @@ function countdown() {
 
     input.value = "";
     input.removeEventListener('input', checkUserInput);
-   
-    gameMusic.pause();
 
+    gameMusic.pause();
+    insanityMusic.pause();
     button.setAttribute('class', 'play-again');
   }
 }
 
-function playbackSpeed(){
-  if(userScore >=5 && userScore <=9 ){
-    gameMusic.playbackRate= 1.2;
+function playbackSpeed() {
+  if (userScore >= 5 && userScore <= 9) {
+    gameMusic.playbackRate = 1.2;
   }
-  if(userScore >= 10){
+  if (userScore >= 10) {
     gameMusic.playbackRate = 1.4;
   }
 }
@@ -123,6 +135,12 @@ function retrieveUserScore() {
   storedUserScore = JSON.parse(getUserScore);
 }
 
+function insanityMode() {
+  gameMusic.pause();
+  insanityMusic.play();
+  insanityMusic.loop = true;
+}
+
 function gameOver() {
   if (storedUserScore.length === 1) {
     alert(`Times up! You scored ${totalScore} points!`)
@@ -134,7 +152,17 @@ function gameOver() {
   } else if (totalScore === storedUserScore[storedUserScore.length - 2]) {
     alert(`You scored ${totalScore} points. No better and no worse!`)
   }
+  
+  // function sortNumber(a,b) {
+  //   return a - b;
+  // }
+
+    var scoreList = document.getElementById('score-list');
+    var listItem = document.createElement('li');
+    listItem.textContent = `User: ${totalScore} points`
+    scoreList.append(listItem);
 }
+
 
 function startGame() {
   input.value = "";
@@ -148,17 +176,22 @@ function startGame() {
 
   countdown();
   generateRandomWord();
+
   gameMusic.play();
   gameMusic.loop = true;
+  gameMusic.playbackRate = 1;
 
   input.addEventListener('input', checkUserInput);
-  log.addEventListener('keydown', function() {
-    incorrectAnswer.play();
-    }
-  );
 };
 
 button.addEventListener("click", function () {
   button.setAttribute('class', 'start');
   startGame();
 });
+
+document.addEventListener('keyup', function () {
+  typewriterSound.pause();
+  typewriterSound.currentTime = 0;
+  typewriterSound.play();
+}
+);
